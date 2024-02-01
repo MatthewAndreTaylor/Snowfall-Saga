d62f158ff80b70122b0c54c725e8abfa2998e647
Matt <matthew.andre.taylor@gmail.com>
2024-02-01 11:51:07 -0500

    [User Story 11, Issue 14] - User database models and authentication

--- app/static/snow.js
+++ app/static/snow.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", () => {
     const snowflakesContainer = document.querySelector(".game-container");
     const snowflakeCount = 200;
 
@@ -7,16 +7,22 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function createSnowflake() {
-        const snowflake = document.createElement('div');
-        var content = document.createTextNode("❄");
+        const snowflake = document.createElement("div");
+        let content = document.createTextNode("❄");
         snowflake.appendChild(content);
-        snowflake.className = 'snowflake';
-        snowflake.style.fontSize = `calc(6px + ${Math.random()*7}px)`;
+        snowflake.className = "snowflake";
+        snowflake.style.fontSize = `calc(6px + ${Math.random() * 7}px)`;
         snowflake.style.left = `${Math.random() * 2000}px`;
         snowflake.style.animation = `
-            snowflake-fall ${6 + 6 * Math.random()}s linear ${1 + 4 * Math.random()}s infinite normal,
-            snowflake-shake ${8 + 6 * Math.random()}s ease-in-out ${1 + 4 * Math.random()}s infinite alternate,
-            snowflake-wind-w ${6 + 6 * Math.random()}s linear ${1 + 4 * Math.random()}s infinite normal
+            snowflake-fall ${6 + 6 * Math.random()}s linear ${
+            1 + 4 * Math.random()
+        }s infinite normal,
+            snowflake-shake ${8 + 6 * Math.random()}s ease-in-out ${
+            1 + 4 * Math.random()
+        }s infinite alternate,
+            snowflake-wind-w ${6 + 6 * Math.random()}s linear ${
+            1 + 4 * Math.random()
+        }s infinite normal
         `;
 
         snowflakesContainer.appendChild(snowflake);
