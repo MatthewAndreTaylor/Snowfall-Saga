3ed3b0a03806e5f6c1a58b31b1bc5bc63f6dbcf3
abhin-T <abhintomar1@gmail.com>
2024-02-01 20:07:10 -0500

    added button to test collision detection

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ players = {}
 clients = set()
 connection_dict = {}
 
-@app.route('/', methods=['GET',"POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("index.html")
 
-@sock.route('/echo')
+
+@sock.route("/echo")
 def echo(connection):
     if connection not in clients:
         clients.add(connection)
@@ -25,29 +27,31 @@ def echo(connection):
             event = connection.receive()
             data = json.loads(event)
 
-            if data['type'] == 'playerUpdate':
-                player_id = data['value']['id']
+            if data["type"] == "playerUpdate":
+                player_id = data["value"]["id"]
                 if player_id not in players:
                     connection_dict[connection] = player_id
-                players[player_id] = data['value']
-                send_to_all_clients({'type': 'playersUpdate', 'value': players})
-            elif data['type'] == 'playerRemoved':
-                player_id = data['id']
-                del players[data['id']]
-                send_to_all_clients({'type': 'playerRemoved', 'id': player_id})
+                players[player_id] = data["value"]
+                send_to_all_clients({"type": "playersUpdate", "value": players})
+            elif data["type"] == "playerRemoved":
+                player_id = data["id"]
+                del players[data["id"]]
+                send_to_all_clients({"type": "playerRemoved", "id": player_id})
 
         except (ConnectionError, ConnectionClosed):
             clients.remove(connection)
             player_id = connection_dict[connection]
-            send_to_all_clients({'type': 'playerRemoved', 'id': player_id})
+            send_to_all_clients({"type": "playerRemoved", "id": player_id})
             del connection_dict[connection]
             del players[player_id]
             print(f"Remove connection {connection}")
             break
 
+
 def send_to_all_clients(message):
     for client in clients:
         client.send(json.dumps(message))
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -2,16 +2,16 @@
 const socket = new WebSocket(`ws://${location.host}/echo`);
 
 function generateUUID() {
-    let uuid = "";
-    const possible = "abcdef0123456789";
-    for (let i = 0; i < 16; i++) {
-        uuid += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return uuid;
+  let uuid = "";
+  const possible = "abcdef0123456789";
+  for (let i = 0; i < 16; i++) {
+    uuid += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return uuid;
 }
 
 function randomChoice(arr) {
-    return arr[Math.floor(arr.length * Math.random())];
+  return arr[Math.floor(arr.length * Math.random())];
 }
 
 const playerColors = ["blue", "red", "orange", "yellow", "green", "purple"];
@@ -24,114 +24,124 @@ const gameContainer = document.querySelector(".game-container");
 const nameInput = document.querySelector("#player-name");
 
 function handleMove(newX, newY) {
-    if (newX > players[playerId].x) {
-        players[playerId].direction = "right";
-    }
-    else if (newX < players[playerId].x) {
-      players[playerId].direction = "left";
-    }
-    players[playerId].x = newX;
-    players[playerId].y = newY;
-    const message = {
-        type: 'playerUpdate',
-        value: players[playerId]
-    };
-    socket.send(JSON.stringify(message));
+  const button = document.querySelector("#trivia").getBoundingClientRect();
+  if (
+    newX < button.right &&
+    newX > button.left &&
+    newY < button.bottom &&
+    newY > button.top
+  ) {
+    return;
+  }
+
+  if (newX > players[playerId].x) {
+    players[playerId].direction = "right";
+  } else if (newX < players[playerId].x) {
+    players[playerId].direction = "left";
+  }
+  players[playerId].x = newX;
+  players[playerId].y = newY;
+  const message = {
+    type: "playerUpdate",
+    value: players[playerId],
+  };
+  socket.send(JSON.stringify(message));
 }
 
 socket.addEventListener("message", (event) => {
-    const data = JSON.parse(event.data);
-    //console.log(data);
+  const data = JSON.parse(event.data);
+  //console.log(data);
 
-    switch (data.type) {
-        case 'playersUpdate':
-            players = data.value || {}
-            Object.keys(players).forEach((key) => {
-                const characterState = players[key];
-                if (key in playerElements) {
-                    let el = playerElements[key];
-                    el.querySelector(".Character_name").innerText = characterState.name;
-                    el.setAttribute("data-color", characterState.color);
-                    el.setAttribute("data-direction", characterState.direction);
-                    const left = characterState.x + "px";
-                    const top = characterState.y + "px";
-                    el.style.transform = `translate3d(${left}, ${top}, 0)`;
-                } else {
-                    const characterElement = document.createElement("div");
-                    characterElement.classList.add("Character", "grid-cell");
-                    characterElement.innerHTML = `
+  switch (data.type) {
+    case "playersUpdate":
+      players = data.value || {};
+      Object.keys(players).forEach((key) => {
+        const characterState = players[key];
+        if (key in playerElements) {
+          let el = playerElements[key];
+          el.querySelector(".Character_name").innerText = characterState.name;
+          el.setAttribute("data-color", characterState.color);
+          el.setAttribute("data-direction", characterState.direction);
+          const left = characterState.x + "px";
+          const top = characterState.y + "px";
+          el.style.transform = `translate3d(${left}, ${top}, 0)`;
+        } else {
+          const characterElement = document.createElement("div");
+          characterElement.classList.add("Character", "grid-cell");
+          characterElement.innerHTML = `
                     <div class="Character_shadow grid-cell"></div>
                     <div class="Character_sprite grid-cell"></div>
                     <div class="Character_name-container">
                         <span class="Character_name">guest</span>
                     </div>
                     <div class="Character_you-arrow"></div>`;
-                    playerElements[characterState.id] = characterElement;
-                    characterElement.setAttribute("data-color", characterState.color);
-                    characterElement.setAttribute("data-direction", characterState.direction);
-                    const left = characterState.x + "px";
-                    const top = characterState.y + "px";
-                    characterElement.style.transform = `translate3d(${left}, ${top}, 0)`;
-                    if (characterState.id === playerId) {
-                      characterElement.classList.add("you");
-                    }
-                    gameContainer.appendChild(characterElement);
-                }
-            });
-            break
-        case 'playerRemoved':
-            const key = data.id;
-            gameContainer.removeChild(playerElements[key]);
-            delete playerElements[key];
-            break
-    }
+          playerElements[characterState.id] = characterElement;
+          characterElement.setAttribute("data-color", characterState.color);
+          characterElement.setAttribute(
+            "data-direction",
+            characterState.direction,
+          );
+          const left = characterState.x + "px";
+          const top = characterState.y + "px";
+          characterElement.style.transform = `translate3d(${left}, ${top}, 0)`;
+          if (characterState.id === playerId) {
+            characterElement.classList.add("you");
+          }
+          gameContainer.appendChild(characterElement);
+        }
+      });
+      break;
+    case "playerRemoved":
+      const key = data.id;
+      gameContainer.removeChild(playerElements[key]);
+      delete playerElements[key];
+      break;
+  }
 });
 
 socket.addEventListener("open", (event) => {
-    console.log("WebSocket connection opened:", event);
-    playerId = generateUUID();
-    const message = {
-      type: 'playerUpdate',
-      value: {
-        id: playerId,
-        name: 'guest',
-        direction: "right",
-        color: randomChoice(playerColors),
-        x: 0,
-        y: 0
-      }
-    };
-    socket.send(JSON.stringify(message));
+  console.log("WebSocket connection opened:", event);
+  playerId = generateUUID();
+  const message = {
+    type: "playerUpdate",
+    value: {
+      id: playerId,
+      name: "guest",
+      direction: "right",
+      color: randomChoice(playerColors),
+      x: 0,
+      y: 0,
+    },
+  };
+  socket.send(JSON.stringify(message));
 });
 
 socket.addEventListener("close", (event) => {
-    console.log("WebSocket connection closed:", event);
-    const message = {
-      type: 'playerRemoved',
-      id: playerId
-    };
-    socket.send(JSON.stringify(message));
+  console.log("WebSocket connection closed:", event);
+  const message = {
+    type: "playerRemoved",
+    id: playerId,
+  };
+  socket.send(JSON.stringify(message));
 });
-
-
 
 nameInput.addEventListener("change", (e) => {
   const newName = e.target.value || "guest";
   nameInput.value = newName;
   const updated_player = players[playerId];
-  updated_player.name = newName
+  updated_player.name = newName;
 
   const message = {
-    type: 'playerUpdate',
-    value: updated_player
+    type: "playerUpdate",
+    value: updated_player,
   };
   socket.send(JSON.stringify(message));
 });
 
 gameContainer.addEventListener("click", (event) => {
-    const clickX = (event.clientX - gameContainer.getBoundingClientRect().left - 16) / 3;
-    const clickY = (event.clientY - gameContainer.getBoundingClientRect().top - 16) / 3;
-    console.log(clickX, clickY)
-    handleMove(clickX, clickY);
-})
-
+  // const clickX = (event.clientX - gameContainer.getBoundingClientRect().left - 16) / 3;
+  // const clickY = (event.clientY - gameContainer.getBoundingClientRect().top - 16) / 3;
+  const clickX = event.clientX - 16;
+  const clickY = event.clientY - 16;
+  handleMove(clickX, clickY);
+});
