8d65b3e4fd315ebb85dbdce789f785b87cefb081
Matthew Taylor <100451342+MatthewAndreTaylor@users.noreply.github.com>
2024-02-03 18:33:32 -0500

    Login Page and auth

--- login_app.py
+++ login_app.py
@@ -1,46 +1,69 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
-from login_db import LoginDB
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, UserMixin, login_required, current_user
+import hmac
 
 app = Flask(__name__)
+app.secret_key = 'MYSECRET'
 
-DATABASE = 'your_database.db'
-db = LoginDB(DATABASE)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
+app.config["SESSION_COOKIE_SECURE"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///accounts.db"
 
-@app.route('/')
-def index():
-    return render_template('login2.html')
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
-@app.route('/login', methods=['POST'])
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id: int):
+    return User.query.filter_by(id=int(user_id)).first()
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
+    if request.method == "GET":
+        return render_template("login.html")
 
-    # Check credentials against the database
-    user = db.login(username, password)
-    #db.close()
+    username = request.form.get('username')
+    password = request.form.get('password')
 
-    if user:
-        # Successful login
-        return f'Welcome, {username}!'
+    user = User.query.filter_by(username=username).first()
+
+    if user and hmac.compare_digest(user.password, password):
+        login_user(user)
+        return redirect('/')
     else:
-        # Invalid credentials, redirect back to the login page
-        return redirect(url_for('index'))
+        return redirect('/login')
+
 
 @app.route('/register', methods=['POST'])
 def register():
-    username = request.form['username']
-    password = request.form['password']
+    username = request.form.get('username')
+    password = request.form.get('password')
 
-    # Check if the username already exists in the database
-    existing_user = db.check_user_exist(username)
+    existing_user = User.query.filter_by(username=username).first()
 
     if existing_user:
-        return 'Username already exists. Please choose another username.'
+        return "Username already exists. Please choose a different username.", 409
     else:
-        # Insert the new user into the database
-        db.register(username, password)
-        return 'Account created successfully. You can now log in.'
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/login')
+
+
+@app.route('/')
+@login_required
+def dashboard():
+    return f"Welcome, {current_user.username}! This is the lobby"
+
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run()
