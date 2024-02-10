7b6e6e3a11a14bb09c0f234e0c2257bd371afd80
Matthew Taylor <100451342+MatthewAndreTaylor@users.noreply.github.com>
2024-02-10 01:40:01 -0500

    [User Story 14, Issue 3] - Fixed project structure

---
+++ lobby_service/app/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = "MYSECRET"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
+    app.config["SESSION_COOKIE_SECURE"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///accounts.db"
+
+    db.init_app(app)
+
+    login_manager.login_view = "login_view.login"
+    login_manager.init_app(app)
+
+    with app.app_context():
+        from .auth import login_view
+
+        app.register_blueprint(login_view)
+
+        from .lobby import lobby_view
+
+        app.register_blueprint(lobby_view)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,11 @@
 import pytest
-import app
+from lobby_service.app import create_app
 
 
 @pytest.fixture
 def client():
-    with app.test_client() as client:
+    test_app = create_app()
+    with test_app.test_client() as client:
         yield client
 
 
@@ -12,3 +13,40 @@ def test_index_page(client):
     response = client.get("/")
     # The index page should redirect new users to login
     assert response.status_code == 302
+
+
+def test_login(client):
+    response = client.get("/login")
+    assert response.status_code == 200
+    assert b"Login" in response.data
+
+    # Create a test user in the database
+    response = client.post(
+        "/register",
+        data={"username": "testuser", "password": "password"},
+        follow_redirects=True,
+    )
+    assert response.status_code == 200
+
+    # Attempt to log in with incorrect credentials
+    response = client.post("/login", data={}, follow_redirects=True)
+    assert response.status_code == 400
+
+    response = client.post(
+        "/login",
+        data={"username": "testuser", "password": "1234"},
+        follow_redirects=True,
+    )
+    assert response.status_code == 401
+
+    response = client.post(
+        "/login", data={"username": "", "password": ""}, follow_redirects=True
+    )
+    assert response.status_code == 401
+
+    response = client.post(
+        "/login",
+        data={"username": "testuser", "password": "password"},
+        follow_redirects=True,
+    )
+    assert response.status_code == 200
