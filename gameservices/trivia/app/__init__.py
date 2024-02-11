b7b0b2892c69c0b0467dda846a3d9badeaf263ca
Jacob Youssef <jacob.youssef@gmail.com>
2024-02-10 20:38:46 -0500

    [User Story 2, Issue 8] - Added the backend for the trivia minigame, including the server and multiplayer functionality

---
+++ gameservices/trivia/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+
+
+def create_trivia_app():
+    app = Flask(
+        __name__,
+        template_folder="templates",
+        static_folder="static",
+    )
+    app.config['SECRET KEY'] = 'secret'
+    socketio = SocketIO(app)
+
+    return app, socketio
+
+
---
+++ gameservices/trivia/run.py
@@ -0,0 +1,4 @@
+import app.trivia_server
+
+if __name__ == '__main__':
+    app.trivia_server.run()
