2d3e9191c7f79daec2f7890a8064b79d56c458b4
Matthew Taylor <100451342+MatthewAndreTaylor@users.noreply.github.com>
2024-02-23 20:08:50 -0500

    [User Story 24, Issue 32] - Created blizzard_bounce game service, fixed css, added sprites and game logic

--- lobby_service/app/lobby/__init__.py
+++ lobby_service/app/lobby/__init__.py
@@ -38,6 +38,12 @@ def trivia():
     return redirect("http://127.0.0.1:9999/trivia")
 
 
+@lobby_view.route("/blizzard_bounce", methods=["GET"])
+@login_required
+def blizzard_bounce():
+    return redirect("http://127.0.0.1:9998")
+
+
 @sock.route("/echo")
 def echo(connection):
     if connection in clients or current_user.id in players:
