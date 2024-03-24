13bcdc68a588e4afe5097b9bb38bdc255ae493fc
Krish Patel <kmp.patel@utoronto.ca>
2024-03-24 12:10:27 -0400

    [User Story 19, Issue 51] - "add chat filtering feature"

---
+++ app/auth/__init__.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_user
+from .. import db, login_manager
+from ..models import User
+
+from better_profanity import profanity
+profanity.load_censor_words_from_file("app/static/bad-words.txt")
+
+login_view = Blueprint(
+    "login_view",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+    static_url_path="/assets/auth",
+)
+
+
+@login_manager.user_loader
+def load_user(user_id: int):
+    return User.query.filter_by(id=int(user_id)).first()
+
+
+@login_view.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("auth/login.html")
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if username is None or password is None:
+        return redirect(url_for("login_view.login"), 400)
+
+    user = User.query.filter_by(username=username).first()
+    # Hash the users password
+    password = User.hash_password(password)
+    if user and user.verify_password(password):
+        login_user(user)
+        return redirect(url_for("lobby_view.lobby"))
+    return redirect(url_for("login_view.login"), 401)
+
+
+@login_view.route("/register", methods=["GET", "POST"])
+def register():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if username is None or password is None:
+        return redirect(url_for("login_view.login"))
+
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        return "Username already exists. Please choose a different username."
+
+    if profanity.contains_profanity(username):
+        return "We are sorry this username goes against our terms of service."
+
+    # Hashing the user's password before adding it to the database
+    password = User.hash_password(password)
+    new_user = User(username=username, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for("login_view.login"))
---
+++ app/messages/__init__.py
@@ -0,0 +1,111 @@
+from datetime import datetime
+from flask import Blueprint
+from flask_login import current_user, login_required
+from simple_websocket import ConnectionClosed
+from flask_sock import Sock
+from collections import deque, defaultdict
+from functools import partial
+import json
+from ..models import User
+
+from better_profanity import profanity
+profanity.load_censor_words_from_file("app/static/bad-words.txt")
+
+messenger = Blueprint(
+    "messenger",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+    static_url_path="/assets/messages",
+)
+
+sock = Sock(messenger)
+
+# Previous messages before the player spawned are saved
+message_caches = defaultdict(partial(deque, maxlen=3))
+
+# Mapping of connections to current user id's
+clients = {}
+users = {}
+
+
+@sock.route("/message")
+@login_required
+def message(connection):
+    send_message(connection, curr_user=current_user)
+
+
+def send_message(connection, curr_user):
+    if connection in clients:
+        return
+
+    # In the case of a test, the current user does not exist
+    if curr_user is None:
+        curr_user = User()
+        curr_user.username = "TestUser"
+        curr_user.id = 1
+
+    clients[connection] = curr_user.username
+    users[curr_user.username] = connection
+
+    # Sort the messages by time
+    missed_mesages = sorted(
+        message_caches["all"] + message_caches[curr_user.username],
+        key=lambda message: message["time"],
+    )
+
+    # Send the last few messages a player missed before they spawned
+    for message in missed_mesages:
+        connection.send(json.dumps(message))
+
+    while True:
+        try:
+            event = connection.receive()
+            data = json.loads(event)
+
+            message_type = data.get("type", "")
+            message_content = data.get("text", "")
+            if profanity.contains_profanity(message_content):
+                message_content = len(message_content) * "*"
+
+            # Create a new message object
+            new_message = {
+                "type": message_type,
+                "text": message_content,
+                "id": curr_user.id,
+                "name": curr_user.username,
+                "time": datetime.now().isoformat(),
+            }
+
+            if message_type == "newMessage":
+                message_caches["all"].append(new_message)
+                send_to_all_clients(new_message)
+
+            elif message_type == "directMessage":
+                to = data.get("to", connection)
+                if connection != users.get(to):
+                    # Show on both your screen and the other person
+                    connection.send(json.dumps(new_message))
+
+                if to in users or User.query.filter_by(username=to).first():
+                    message_caches[to].append(new_message)
+
+                if to in users:
+                    users[to].send(json.dumps(new_message))
+
+        except (KeyError, ConnectionError, ConnectionClosed):
+            user = clients[connection]
+            del clients[connection]
+            del users[user]
+            print(f"Messenger Removed connection {connection}")
+            break
+
+
+def send_to_all_clients(message: dict):
+    """Sends a message to all connected clients.
+
+    Args:
+        message (dict): The message to be sent.
+    """
+    for client in clients:
+        client.send(json.dumps(message))
