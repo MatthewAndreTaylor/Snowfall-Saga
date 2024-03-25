375b6e710a5b58096f93f23678a80de805cbc519
Matthew Taylor <100451342+MatthewAndreTaylor@users.noreply.github.com>
2024-03-24 22:25:18 -0400

    [User Story 17, Issue 53] - Snowfall updates

--- lobby_service/app/static/snow.js
+++ lobby_service/app/static/snow.js
@@ -1,30 +1,64 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const snowflakesContainer = document.querySelector(".game-container");
-  const snowflakeCount = 200;
+const canvas = document.getElementById("snowfall");
+const ctx = canvas.getContext("2d");
+let snowflakes = [];
 
-  for (let i = 0; i < snowflakeCount; i++) {
-    createSnowflake();
+function setCanvasSize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+function createSnowflakes() {
+  snowflakes = [];
+  for (let i = 0; i < 50; i++) {
+    snowflakes.push({
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      opacity: Math.random(),
+      speedX: (Math.random() - 0.5) * 5,
+      speedY: Math.random() + 1,
+      radius: Math.random() * 5 + 1,
+    });
   }
+}
 
-  function createSnowflake() {
-    const snowflake = document.createElement("div");
-    let content = document.createTextNode("❄");
-    snowflake.appendChild(content);
-    snowflake.className = "snowflake";
-    snowflake.style.fontSize = `calc(6px + ${Math.random() * 7}px)`;
-    snowflake.style.left = `${Math.random() * 90}vw`;
-    snowflake.style.animation = `
-            snowflake-fall ${6 + 6 * Math.random()}s linear ${
-              1 + 4 * Math.random()
-            }s infinite normal,
-            snowflake-shake ${8 + 6 * Math.random()}s ease-in-out ${
-              1 + 4 * Math.random()
-            }s infinite alternate,
-            snowflake-wind-w ${6 + 6 * Math.random()}s linear ${
-              1 + 4 * Math.random()
-            }s infinite normal
-        `;
+function drawSnowflakes() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgba(200, 200, 255, 0.8)";
+  ctx.beginPath();
+  snowflakes.forEach(flake => {
+    ctx.moveTo(flake.x, flake.y);
+    ctx.arc(flake.x, flake.y, flake.radius, 0, Math.PI * 2);
+  });
+  ctx.fill();
+}
 
-    snowflakesContainer.appendChild(snowflake);
-  }
+function moveSnowflakes() {
+  console.log("Here")
+  snowflakes.forEach(flake => {
+    flake.x += flake.speedX;
+    flake.y += flake.speedY;
+    if (flake.y > canvas.height) {
+        flake.x = Math.random() * canvas.width;
+        flake.y = -flake.radius * 2;
+    }
+  });
+}
+
+function updateSnowfall() {
+  moveSnowflakes();
+  drawSnowflakes();
+  requestAnimationFrame(updateSnowfall);
+}
+
+let resizeEnd;
+window.addEventListener("resize", () => {
+  clearTimeout(resizeEnd);
+  resizeEnd = setTimeout(() => {
+    setCanvasSize();
+    createSnowflakes();
+  }, 200);
 });
+
+setCanvasSize();
+createSnowflakes();
+updateSnowfall();
